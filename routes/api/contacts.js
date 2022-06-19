57d668d9ae13a46b67986971f33d62c2ba92ecc3
LesiaBelinska <lm.kudak@gmail.com>
2022-06-19 03:01:49 +0300

    add contactAddSchema for validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
 
 const contacts = require('../../models/contacts');
 
 const { createError } = require("../../helpers");
 
 const router = express.Router()
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
 
 router.get('/', async (req, res, next) => {
   try {
@@ -41,6 +48,10 @@ router.get('/:contactId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
