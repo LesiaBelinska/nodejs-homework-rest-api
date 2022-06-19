425f34c1bd56a850498c465ebe10221f823b074c
LesiaBelinska <lm.kudak@gmail.com>
2022-06-19 04:03:26 +0300

    Create updateContact.js

---
+++ controllers/contacts/updateContact.js
@@ -0,0 +1,19 @@
+const contacts = require('../../models/contacts');
+const { createError } = require("../../helpers");
+const { addSchema } = require("../../schemas/contacts");
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = updateContact;
