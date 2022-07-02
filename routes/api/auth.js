99b4d83dcf63f7b0ad9c2244963494ee3fe63cf5
LesiaBelinska <lm.kudak@gmail.com>
2022-07-02 23:58:04 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.post("/signup", validation(schemas.signup), ctrl.signup);
 
-
+router.post("/login", validation(schemas.signup), ctrl.login);
 
 module.exports = router;
