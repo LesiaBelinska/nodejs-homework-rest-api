d388af9e9d880765af770cdd0f0d6078d6bfe6b2
LesiaBelinska <lm.kudak@gmail.com>
2022-07-03 01:41:50 +0300

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,9 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res, next) => {
   try {
-    const result = await Contact.find({}, "-createdAt -updatedAt");
+    const { _id: owner } = req.user;
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt")
+      .populate("owner", "_id email subscription");
     res.json(result);
   } catch (error) {
     next(error);
