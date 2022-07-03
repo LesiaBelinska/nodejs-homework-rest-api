7f1321bc2ae071d928f1bac66f02223e428f0db0
LesiaBelinska <lm.kudak@gmail.com>
2022-07-03 10:05:50 +0300

    add getCurrent

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validation } = require("../../middlewares");
+const { validation, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -11,5 +11,7 @@ const router = express.Router();
 router.post("/signup", validation(schemas.signup), ctrl.signup);
 
 router.post("/login", validation(schemas.signup), ctrl.login);
+
+router.get("./current", authenticate, ctrl.getCurrent);
 
 module.exports = router;
