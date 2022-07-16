ff5e91ae345f9155be131867bcf4b465b324c686
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-16 21:19:18 +0300

    Update index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,12 +2,14 @@ const signup = require("./signup");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
-const updateAvatar =require("./updateAvatar");
+const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
 
 module.exports = {
-    signup,
-    login,
-    getCurrent,
-    logout,
-    updateAvatar,
-}
+  signup,
+  login,
+  getCurrent,
+  logout,
+  updateAvatar,
+  verifyEmail,
+};
