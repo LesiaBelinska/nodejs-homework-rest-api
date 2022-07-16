1d442698ce3c8491a28e718095dc024097ef03c4
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-16 21:26:38 +0300

    add resend verify email router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,6 +12,8 @@ router.post("/signup", validation(schemas.signup), ctrl.signup);
 
 router.get("/verify/:verificationToken", ctrl.verifyEmail);
 
+router.post("/verify", validation(schemas.email), ctrl.resendVerifyEmail);
+
 router.post("/login", validation(schemas.signup), ctrl.login);
 
 router.get("/logout", authenticate, ctrl.logout);
